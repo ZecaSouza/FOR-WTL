2043cd5e686645963e76a511a2c6925b71946287
ZecaBorges <jose.borges@redspark.io>
2023-10-10 09:01:16 -0300

    classe cargo

--- src/main/java/com/Test_WTL/Test_WTL/entidades/DepartamentoEntity.java
+++ src/main/java/com/Test_WTL/Test_WTL/entidades/DepartamentoEntity.java
@@ -2,7 +2,9 @@ package com.Test_WTL.Test_WTL.entidades;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_departamentos")
@@ -11,12 +13,23 @@ public class DepartamentoEntity extends AbstractEntity<Long>{
     @Column(name = "nome", nullable = false, unique = true, length = 60)
     private String nome;
 
+    @OneToMany(mappedBy = "departamento")
+    private List<CargoEntity> cargo;
+
     public String getNome() {
         return nome;
     }
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public List<CargoEntity> getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(List<CargoEntity> cargo) {
+        this.cargo = cargo;
     }
 
 }
